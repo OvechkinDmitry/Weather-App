1301cf0b276652cade02eebe464fefc5e3d5f989
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2022-10-20 12:18:38 +0500

    created header component

---
+++ src/app/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './app.css'
+import {Header} from "../widgets/Header/header";
+export const App = () =>{
+    return (<div className="app-container">
+                     <Header/>
+                    <div>привет</div>
+            </div>
+    )
+}
---
+++ src/widgets/Header/header.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import './header.css'
+export const Header = () =>  {
+        return (
+            <div className="header">
+              Weather App
+            </div>
+        );
+}
