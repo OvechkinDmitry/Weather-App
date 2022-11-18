614da7a05bb19d72783def900835ebde5e2af920
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2022-11-18 19:50:31 +0500

    styles for screens

--- src/shared/map/map.js
+++ src/shared/map/map.js
@@ -5,14 +5,22 @@ import {LoadingRing} from "../ui/loading-ring/loading-ring";
 
 export const CardMap = ({lat,lon}) => {
     const [isLoading,setIsLoading] = useState(true)
+    const styleMap = {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        width: '100%',
+        height: '100%',
+        padding: "0 5px",
+    };
+    const styleMapContainer = {width:"500", height: "600",position:"relative"};
     return (
-        <div className="card-body__map">
+        <div className="card-body__map" style={styleMapContainer}>
             {isLoading && <LoadingRing/>}
             <YMaps>
                 <Map
                     onLoad={() => setIsLoading(false)}
-                    width={500}
-                    height={500}
+                    style={styleMap}
                     defaultState={{center: [lat, lon], zoom: 9}}
                 >
                     <Placemark geometry={[lat, lon]}/>
